af8f45b7e5b062dc13e0c9e89948b2cc454265b1
kevin.dean@utsouthwestern.edu <kevin.dean@utsouthwestern.edu>
2021-01-29 14:58:37 -0600

    multinode slurm

--- shell_scripts/compile_flowscape.sh
+++ shell_scripts/compile_flowscape.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 # A program that attempts to compile the Flowscape software.
+# Previously tried cmake 2.8.12.  Failed.
+# The base for gcc is 4.8.5.  Previously tried 5.4.0.
 
 module purge;
-# previously tried cmake/2.8.12.  
 module load shared slurm cmake/3.10.2;
-
-# base gcc/4.8.5.  Previously tried 5.4.0
 module load gcc/8.3.0;
-module list;
+
 rm -R /home2/kdean/Applications/flowscope/flowscope/CSL2/build/*;
 cd /home2/kdean/Applications/flowscope/flowscope/CSL2/build;
 
